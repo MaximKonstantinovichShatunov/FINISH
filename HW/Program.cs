2b8e43b9706b2a86ca719e652b34d3d9a552b175
maxim <shatunov-87@mail.ru>
2022-12-14 20:01:43 +0300

    zadacha cs

---
+++ HW/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] array = new string[] { "1234", "157", "-2", "computer", "PIG", "999", "777" };
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length < 4)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
